33153bacfcdfa601bb1e9a5d49fa85298c697c04
Mohamed Hegazy <mhegazy@microsoft.com>
2015-09-02 17:12:59 -0700

    Fix formatting and spaces

--- jsx/src/app.tsx
+++ jsx/src/app.tsx
@@ -3,19 +3,19 @@ import $ = require('jquery');
 import {Greeter as Greetifier, GreeterProps as GreeterProps} from 'greeter';
 
 function getRandomGreeting() {
-	switch(Math.floor(Math.random() * 4)) {
-		case 0: return 'Hello';
-		case 1: return 'Howdy';
-		case 2: return 'Greetings to you';
-		case 3: return 'Hail';
-	}
+    switch (Math.floor(Math.random() * 4)) {
+        case 0: return 'Hello';
+        case 1: return 'Howdy';
+        case 2: return 'Greetings to you';
+        case 3: return 'Hail';
+    }
 }
 
 $(() => {
-	let props: GreeterProps = {
-		whomToGreet: 'world!',
-	};
+    let props: GreeterProps = {
+        whomToGreet: 'world!',
+    };
 
-	React.render(<Greetifier {...props} />, $('#output').get(0));
+    React.render(<Greetifier {...props} />, $('#output').get(0));
 });
 
