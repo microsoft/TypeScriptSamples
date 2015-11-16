8bd1c1836cc3c1c4644bc39b4d038850e0af3b3b
Daniel Rosenwasser <drosen@microsoft.com>
2015-11-16 14:03:26 -0800

    Updated typings.

---
+++ imageboard/typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="express/express.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="mongodb/mongodb.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
