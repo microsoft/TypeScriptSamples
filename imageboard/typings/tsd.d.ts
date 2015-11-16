41a3ee10ee780f67e0ccabb064e2943429f58059
Daniel Rosenwasser <drosen@microsoft.com>
2015-11-16 14:35:23 -0800

    Updated typings, use a tsd.json.

--- imageboard/typings/tsd.d.ts
+++ imageboard/typings/tsd.d.ts
@@ -3,3 +3,6 @@
 /// <reference path="mongodb/mongodb.d.ts" />
 /// <reference path="node/node.d.ts" />
 /// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="errorhandler/errorhandler.d.ts" />
+/// <reference path="method-override/method-override.d.ts" />
