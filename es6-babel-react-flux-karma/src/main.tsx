9a98b366d1cfc62d0dc5595d03a46b24bdd9c6a6
John Reilly <johnny_reilly@hotmail.com>
2016-02-16 06:23:59 +0000

    Remove use React as an expression

--- es6-babel-react-flux-karma/src/main.tsx
+++ es6-babel-react-flux-karma/src/main.tsx
@@ -2,6 +2,5 @@ import 'babel-polyfill';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
-React; // use React as an expression to prevent React being purged from dependencies as not used directly
 
 ReactDOM.render(<App />, document.getElementById('content'));
