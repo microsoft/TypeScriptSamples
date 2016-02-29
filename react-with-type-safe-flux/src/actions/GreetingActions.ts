4935670d1e70274bc8534945e0e514eb97009414
Yaroslav <jlarky@gmail.com>
2016-02-29 00:20:30 -0800

    add payload types

--- react-with-type-safe-flux/src/actions/GreetingActions.ts
+++ react-with-type-safe-flux/src/actions/GreetingActions.ts
@@ -1,23 +1,27 @@
 import {Event, AppDispatcher} from '../dispatcher/AppDispatcher';
 import GreetingActionTypes from '../constants/action-types/GreetingActionTypes';
 
+export interface AddGreetingEvent {type: string; payload: string;}
+export interface NewGreetingChanged {type: string; payload: string;}
+export interface RemoveGreeting {type: string; payload: string;}
+
 export function addGreeting(newGreeting: string) {
   AppDispatcher.dispatch({
     payload: newGreeting,
     type: GreetingActionTypes.ADD_GREETING
-  });
+  } as AddGreetingEvent);
 }
 
 export function newGreetingChanged(newGreeting: string) {
   AppDispatcher.dispatch({
     payload: newGreeting,
     type: GreetingActionTypes.NEW_GREETING_CHANGED
-  });
+  } as NewGreetingChanged);
 }
 
 export function removeGreeting(greetingToRemove: string) {
   AppDispatcher.dispatch({
     payload: greetingToRemove,
     type: GreetingActionTypes.REMOVE_GREETING
-  });
+  } as RemoveGreeting);
 }
--- react-with-type-safe-flux/src/stores/GreetingStore.ts
+++ react-with-type-safe-flux/src/stores/GreetingStore.ts
@@ -2,22 +2,26 @@ import FluxStore from './FluxStore';
 import GreetingActionTypes from '../constants/action-types/GreetingActionTypes';
 import {Event, AppDispatcher} from '../dispatcher/AppDispatcher';
 import GreetingState from '../types/GreetingState';
+import { AddGreetingEvent, RemoveGreeting, NewGreetingChanged } from '../actions/GreetingActions';
 
 class GreeterStore extends FluxStore<GreetingState> {
   constructor(dispatcher: Flux.Dispatcher<Event>) {
     const onDispatch = (action: Event) => {
       switch(action.type) {
         case GreetingActionTypes.ADD_GREETING:
+          let payload1 = (<AddGreetingEvent> action).payload;
           this._state.newGreeting = '';
-          this._state.greetings = this._state.greetings.concat(action.payload);
+          this._state.greetings = this._state.greetings.concat(payload1);
           this.emitChange();
           break;
         case GreetingActionTypes.REMOVE_GREETING:
-          this._state.greetings = this._state.greetings.filter(g => g !== action.payload);
+          let payload2 = (<RemoveGreeting> action).payload;
+          this._state.greetings = this._state.greetings.filter(g => g !== payload2);
           this.emitChange();
           break;
         case GreetingActionTypes.NEW_GREETING_CHANGED:
-          this._state.newGreeting = action.payload;
+          let payload3 = (<NewGreetingChanged> action).payload;
+          this._state.newGreeting = payload3;
           this.emitChange();
           break;
       }
